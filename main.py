d1e40d0256650439063ed3847b7f4ad8aa7e9977
Mahmoud Diab <mahmouddeiab20142@gmail.com>
2025-02-01 06:47:50 +0200

    feat: represent tokens in a data frame.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Step 1. Load a tokenizer and a model
@@ -5,3 +6,8 @@ tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
 inputs = tokenizer("Hi everyone, I'm Diab, and I'm studying Generative AI", return_tensors = "pt")
 print(inputs["input_ids"])
+
+# Step 2. Examine the tokenization¶
+token_tuples = [(id, tokenizer.decode(id))for id in inputs["input_ids"][0]]
+table = pandas.DataFrame(token_tuples, columns=("id", "token"))
+print(table)
